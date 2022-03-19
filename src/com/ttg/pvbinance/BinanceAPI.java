5aa3bbcf619c4dd67e9003d9c6f4ddfb8d715a13
GregorSomething <55884713+GregorSomething@users.noreply.github.com>
2022-03-19 10:48:36 +0200

    Update

--- src/com/ttg/pvbinance/BinanceAPI.java
+++ src/com/ttg/pvbinance/BinanceAPI.java
@@ -7,5 +7,18 @@ import com.binance.connector.client.impl.SpotClientImpl;
  */
 
 public class BinanceAPI {
-    SpotClientImpl spotClient = new SpotClientImpl(PrivateConf.API_KEY, PrivateConf.SECRET_KEY, PrivateConf.baseUrl);
+
+    private SpotClientImpl spotClient;
+    private static BinanceAPI instance;
+
+    public BinanceAPI() {
+        BinanceAPI.instance = this;
+        this.spotClient = new SpotClientImpl(PrivateConf.API_KEY, PrivateConf.SECRET_KEY, PrivateConf.baseUrl);
+        System.out.println(this.spotClient.createMarket().time());
+    }
+
+
+    public static BinanceAPI getInstance() {
+        return BinanceAPI.instance;
+    }
 }
--- src/com/ttg/pvbinance/Main.java
+++ src/com/ttg/pvbinance/Main.java
@@ -3,6 +3,7 @@ package com.ttg.pvbinance;
 public class Main {
 
     public static void main(String[] args) {
+        new BinanceAPI();
 	// write your code here
     }
 }

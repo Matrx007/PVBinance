767ee969823db2b84b831c5969501f9f0f906054
GregorSomething <55884713+GregorSomething@users.noreply.github.com>
2022-03-19 12:26:49 +0200

    Update

--- src/main/java/com/ttg/pvbinance/BinanceAPI.java
+++ src/main/java/com/ttg/pvbinance/BinanceAPI.java
@@ -2,6 +2,7 @@ package com.ttg.pvbinance;
 
 import com.binance.connector.client.impl.SpotClientImpl;
 import com.binance.connector.client.impl.spot.Market;
+import com.binance.connector.client.impl.spot.Wallet;
 import com.binance.connector.client.utils.JSONParser;
 
 import java.util.HashMap;
@@ -21,19 +22,28 @@ public class BinanceAPI {
         this.spotClient = new SpotClientImpl(PrivateConf.API_KEY, PrivateConf.SECRET_KEY, PrivateConf.baseUrl);
         System.out.println(this.getTime());
         this.getCurrencies();
+        this.getCurrencyInOther("BTCBUSD", "ETHBUSD");
     }
 
     public long getTime() {
-        return Long.parseLong(JSONParser.getJSONStringValue(spotClient.createMarket().time(), "serverTime"));
+        return Long.parseLong(spotClient.createMarket().time().replace("{\"serverTime\":", "").replace("}", ""));
     }
 
     public HashMap<String, String> getCurrencies() {
-        String data = this.spotClient.createWallet().coinInfo(new LinkedHashMap<String, Object>());
+        // https://binance-docs.github.io/apidocs/spot/en/#all-coins-39-information-user_data
+        LinkedHashMap<String, Object> prop = new LinkedHashMap<>();
+        prop.put("timestamp", this.getTime());
+        Wallet wallet = this.spotClient.createWallet();
+        String data = wallet.coinInfo(prop);
         return new HashMap<String, String>();
     }
 
     public void getCurrencyInOther(String first, String second) {
-        System.out.println(this.spotClient.createMarket().tickerSymbol(new LinkedHashMap<String, Object>()));
+        LinkedHashMap<String, Object> prop = new LinkedHashMap<>();
+        prop.put("symbol", first);
+        System.out.println(this.spotClient.createMarket().tickerSymbol(prop));
+        prop.replace("symbol", second);
+        System.out.println(this.spotClient.createMarket().tickerSymbol(prop));
     }
 
 

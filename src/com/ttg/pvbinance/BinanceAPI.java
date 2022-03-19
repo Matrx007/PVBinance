edc22ed3837ab1a4fb42fd7ebc58e9d55b0d5390
GregorSomething <55884713+GregorSomething@users.noreply.github.com>
2022-03-19 10:36:51 +0200

    Update

--- src/com/ttg/pvbinance/BinanceAPI.java
+++ src/com/ttg/pvbinance/BinanceAPI.java
@@ -1,8 +1,11 @@
 package com.ttg.pvbinance;
 
+import com.binance.connector.client.impl.SpotClientImpl;
+
 /**
  * This class will contain high-level functions which will call the Binance API.
  */
 
 public class BinanceAPI {
+    SpotClientImpl spotClient = new SpotClientImpl(PrivateConf.API_KEY, PrivateConf.SECRET_KEY, PrivateConf.baseUrl);
 }

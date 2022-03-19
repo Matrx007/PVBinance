24a2438a0a6a1cdd5ae41e30fcf50f538a79a064
GregorSomething <55884713+GregorSomething@users.noreply.github.com>
2022-03-19 13:29:37 +0200

    Update BinanceAPI.java

--- src/main/java/com/ttg/pvbinance/BinanceAPI.java
+++ src/main/java/com/ttg/pvbinance/BinanceAPI.java
@@ -2,9 +2,11 @@ package com.ttg.pvbinance;
 
 import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.domain.general.SymbolInfo;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * This class will contain high-level functions which will call the Binance API.
@@ -26,35 +28,15 @@ public class BinanceAPI { // https://github.com/binance-exchange/binance-java-ap
     }
 
     public long getTime() {
-        client.ge
-        return Long.parseLong(spotClient.createMarket().time().replace("{\"serverTime\":", "").replace("}", ""));
+        return client.getServerTime();
     }
 
-    public ArrayList<CryptoPriceInfo> getCurrencies() {
-        /* // https://binance-docs.github.io/apidocs/spot/en/#all-coins-39-information-user_data
-        LinkedHashMap<String, Object> prop = new LinkedHashMap<>();
-        prop.put("timestamp", this.getTime());
-        Wallet wallet = this.spotClient.createWallet();
-        String data = wallet.coinInfo(prop);
-        System.out.println(data);
-        return new HashMap<String, String>();
-        */
-        String data = this.spotClient.createMarket().tickerSymbol(new LinkedHashMap<>());
-        ArrayList<CryptoPriceInfo> priceInfos = new ArrayList<>();
-        for (String s : data.substring(1, data.length() - 2).split("}(,)*")) {
-            s = s + "}";
-            priceInfos.add(new CryptoPriceInfo(JSONParser.getJSONStringValue(s, "symbol"),
-                    Float.parseFloat(JSONParser.getJSONStringValue(s, "price"))));
-        }
-        return priceInfos;
+    public List<SymbolInfo> getCurrencies() {
+        return client.getExchangeInfo().getSymbols();
     }
 
     public void getCurrencyInOther(String first, String second) {
-        LinkedHashMap<String, Object> prop = new LinkedHashMap<>();
-        prop.put("symbol", first);
-        System.out.println(this.spotClient.createMarket().tickerSymbol(prop));
-        prop.replace("symbol", second);
-        System.out.println(this.spotClient.createMarket().tickerSymbol(prop));
+        
     }
 
 

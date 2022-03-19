3af8333b04a10a81925defb7f0295b4cf4d2e719
Matrx007 <raintz1000@gmail.com>
2022-03-19 10:17:41 +0200

    Basic project structure

---
+++ src/com/ttg/pvbinance/BinanceAPI.java
@@ -0,0 +1,8 @@
+package com.ttg.pvbinance;
+
+/**
+ * This class will contain high-level functions which will call the Binance API.
+ */
+
+public class BinanceAPI {
+}
---
+++ src/com/ttg/pvbinance/GUI.java
@@ -0,0 +1,8 @@
+package com.ttg.pvbinance;
+
+/**
+ * This class connects all GUI components. GUI will be started from here.
+ */
+
+public class GUI {
+}

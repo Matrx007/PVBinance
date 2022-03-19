721e002f727b15fbb322b670327abc470d59227b
GregorSomething <55884713+GregorSomething@users.noreply.github.com>
2022-03-19 12:06:10 +0200

    Update

--- src/main/java/com/ttg/pvbinance/BinanceAPI.java
+++ src/main/java/com/ttg/pvbinance/BinanceAPI.java
@@ -2,6 +2,10 @@ package com.ttg.pvbinance;
 
 import com.binance.connector.client.impl.SpotClientImpl;
 import com.binance.connector.client.impl.spot.Market;
+import com.binance.connector.client.utils.JSONParser;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 /**
  * This class will contain high-level functions which will call the Binance API.
@@ -15,8 +19,23 @@ public class BinanceAPI {
     public BinanceAPI() {
         BinanceAPI.instance = this;
         this.spotClient = new SpotClientImpl(PrivateConf.API_KEY, PrivateConf.SECRET_KEY, PrivateConf.baseUrl);
-        Market market = spotClient.createMarket();
+        System.out.println(this.getTime());
+        this.getCurrencies();
     }
+
+    public long getTime() {
+        return Long.parseLong(JSONParser.getJSONStringValue(spotClient.createMarket().time(), "serverTime"));
+    }
+
+    public HashMap<String, String> getCurrencies() {
+        String data = this.spotClient.createWallet().coinInfo(new LinkedHashMap<String, Object>());
+        return new HashMap<String, String>();
+    }
+
+    public void getCurrencyInOther(String first, String second) {
+        System.out.println(this.spotClient.createMarket().tickerSymbol(new LinkedHashMap<String, Object>()));
+    }
+
 
 
     public static BinanceAPI getInstance() {
